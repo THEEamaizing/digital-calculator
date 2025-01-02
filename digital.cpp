5e370a3c0d804706e7a9cccf404ce12b3a9a97b8
THEEamaizing <ambimwaluseke@gmail.com>
2025-01-02 15:46:49 +0300

    Add displayMenu function

--- digital.cpp
+++ digital.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <iomanip>
+#include <limits>
+#include <cmath>
 using namespace std;
 
 void displayMenu() {
-    cout << "\nSimple Calculator" << endl;
-    cout << "-------------------" << endl;
+    cout << "\n==== Advanced Calculator Application ====" << endl;
     cout << "1. Addition" << endl;
     cout << "2. Subtraction" << endl;
     cout << "3. Multiplication" << endl;
     cout << "4. Division" << endl;
-    cout << "5. Exit" << endl;
-    cout << "Choose an operation (1-5): ";
+    cout << "5. Modulus" << endl;
+    cout << "6. Power" << endl;
+    cout << "7. Square Root" << endl;
+    cout << "8. Exit" << endl;
+    cout << "Choose an operation (1-8): ";
 }
