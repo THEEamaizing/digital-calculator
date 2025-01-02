4b85b63a33774992f4386b980cfec23fb0bd651f
THEEamaizing <ambimwaluseke@gmail.com>
2025-01-02 15:43:49 +0300

    Add essential #include directives

---
+++ digital.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void displayMenu() {
+    cout << "\nSimple Calculator" << endl;
+    cout << "-------------------" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Choose an operation (1-5): ";
+}
