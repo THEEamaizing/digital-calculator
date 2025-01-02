2a4b1f127ef93c4552ce24cfb3f7f37c5955b2f4
THEEamaizing <ambimwaluseke@gmail.com>
2025-01-02 17:54:07 +0300

    updating code

--- digital.cpp
+++ digital.cpp
@@ -60,3 +60,10 @@ if (choice == 3) {
     result = num1 * num2;
     cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
 }
+  else if (choice == 3) {
+            double num1, num2, result;
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+            result = num1 * num2;
+            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
+  }
